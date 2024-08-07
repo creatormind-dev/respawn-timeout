b6a40d89c018a3f7aafa38955bd12a46923d6ff5
Mind <63318104+creatormind-dev@users.noreply.github.com>
2024-08-07 17:06:11 -0600

    Fix out of range values for different time units

--- src/main/java/dev/creatormind/respawntimeout/commands/SetCommand.java
+++ src/main/java/dev/creatormind/respawntimeout/commands/SetCommand.java
@@ -18,51 +18,83 @@ public class SetCommand {
 
     private static final int ADMIN_PERMISSION_LEVEL = 4;
 
+    private static final long MAX_TIMEOUT_IN_SECONDS = 604800L;
+    private static final long MAX_TIMEOUT_IN_MINUTES = 10080L;
+    private static final long MAX_TIMEOUT_IN_HOURS = 168L;
+    private static final long MAX_TIMEOUT_IN_DAYS = 7L;
+
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         // Registration call for configuration in seconds.
         dispatcher.register(literal("respawntimeout")
             .then(literal("set")
                 .requires((source) -> source.hasPermissionLevel(ADMIN_PERMISSION_LEVEL))
-                .then(argument("timeout", longArg(0L, 604800L))
-                    .then(literal("seconds")
-                        .executes((context) -> set(context.getSource(), getLong(context, "timeout"), TimeUnit.SECONDS))
-                    ))));
+                .then(literal("fixed")
+                    .then(argument("timeout", longArg(0L, MAX_TIMEOUT_IN_SECONDS))
+                        .then(literal("seconds")
+                            .executes((context) -> set(context.getSource(), getLong(context, "timeout"), MAX_TIMEOUT_IN_SECONDS, TimeUnit.SECONDS))
+                        )))));
 
         // Registration call for configuration in minutes.
         dispatcher.register(literal("respawntimeout")
             .then(literal("set")
                 .requires((source) -> source.hasPermissionLevel(ADMIN_PERMISSION_LEVEL))
-                .then(argument("timeout", longArg(0L, 10080L))
-                    .then(literal("minutes")
-                        .executes((context) -> set(context.getSource(), getLong(context, "timeout"), TimeUnit.MINUTES))
-                    ))));
+                .then(literal("fixed")
+                    .then(argument("timeout", longArg(0L, MAX_TIMEOUT_IN_MINUTES))
+                        .then(literal("minutes")
+                            .executes((context) -> set(context.getSource(), getLong(context, "timeout"), MAX_TIMEOUT_IN_MINUTES, TimeUnit.MINUTES))
+                        )))));
 
         // Registration call for configuration in hours.
         dispatcher.register(literal("respawntimeout")
             .then(literal("set")
                 .requires((source) -> source.hasPermissionLevel(ADMIN_PERMISSION_LEVEL))
-                .then(argument("timeout", longArg(0L, 168L))
-                    .then(literal("hours")
-                        .executes((context) -> set(context.getSource(), getLong(context, "timeout"), TimeUnit.HOURS))
-                    ))));
+                .then(literal("fixed")
+                    .then(argument("timeout", longArg(0L, MAX_TIMEOUT_IN_HOURS))
+                        .then(literal("hours")
+                            .executes((context) -> set(context.getSource(), getLong(context, "timeout"), MAX_TIMEOUT_IN_HOURS, TimeUnit.HOURS))
+                        )))));
 
         // Registration call for configuration in days.
         dispatcher.register(literal("respawntimeout")
             .then(literal("set")
                 .requires((source) -> source.hasPermissionLevel(ADMIN_PERMISSION_LEVEL))
-                .then(argument("timeout", longArg(0L, 7L))
-                    .then(literal("days")
-                        .executes((context) -> set(context.getSource(), getLong(context, "timeout"), TimeUnit.DAYS))
-                    ))));
+                .then(literal("fixed")
+                    .then(argument("timeout", longArg(0L, MAX_TIMEOUT_IN_DAYS))
+                        .then(literal("days")
+                            .executes((context) -> set(context.getSource(), getLong(context, "timeout"), MAX_TIMEOUT_IN_DAYS, TimeUnit.DAYS))
+                        )))));
     }
 
-    private static int set(ServerCommandSource source, long timeout, TimeUnit unit) throws CommandSyntaxException {
+    private static int set(ServerCommandSource source, long timeout, long maxTimeout, TimeUnit unit) throws CommandSyntaxException {
         // No comment.
         if (timeout < 0L)
             throw new SimpleCommandExceptionType(Text.translatable("respawn-timeout.commands.set.error.negative")).create();
 
         final ServerState serverState = ServerState.getServerState(source.getServer());
+
+        // Throws an error in case the timeout in a time unit is out of range.
+        switch (unit) {
+            case SECONDS:
+                if (timeout <= MAX_TIMEOUT_IN_SECONDS)
+                    break;
+            case MINUTES:
+                if (timeout <= MAX_TIMEOUT_IN_MINUTES)
+                    break;
+            case HOURS:
+                if (timeout <= MAX_TIMEOUT_IN_HOURS)
+                    break;
+            case DAYS:
+                if (timeout <= MAX_TIMEOUT_IN_DAYS)
+                    break;
+            default:
+                throw new SimpleCommandExceptionType(Text.translatable(
+                    "respawn-timeout.commands.set.error.invalid",
+                    unit.toString().toUpperCase(),
+                    0L,
+                    maxTimeout
+                )).create();
+        }
 
         serverState.respawnTimeout = timeout;
         serverState.timeUnit = unit;
